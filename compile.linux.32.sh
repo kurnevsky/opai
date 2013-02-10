d3c2a70f7f0b799a673905d5c439352a16e2e70c
Kurnevsky Evgeny <kurnevsky@gmail.com>
2013-02-10 15:35:10 +0300

    Clarification of architecture.

--- compile.linux.32.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-g++ -o opai -Wall -Wcast-qual -Wextra -Wshadow -pedantic -m32 -msse2 -flto -lrt -lboost_random -std=c++0x -fopenmp -Ofast -DLINUX -DENVIRONMENT_32 main.cpp bot.cpp field.cpp minimax.cpp position_estimate.cpp uct.cpp
---
+++ compile.linux.x86-32.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+g++ -o opai-linux-x86-32 -Wall -Wcast-qual -Wextra -Wshadow -pedantic -m32 -msse2 -flto -lrt -lboost_random -std=c++0x -fopenmp -Ofast -DLINUX -DENVIRONMENT_32 main.cpp bot.cpp field.cpp minimax.cpp position_estimate.cpp uct.cpp
