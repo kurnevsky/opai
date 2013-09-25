72183272580aea331530136bea291383610f656f
kurnevsky <kurnevsky@gmail.com>
2013-09-25 17:39:37 +0300

    Refactoring.

--- zobrist.h
+++ zobrist.h
@@ -13,18 +13,18 @@ class Zobrist
 {
 private:
   // Размер таблицы хешей.
-  int _size;
+  Pos _size;
   // Хеши.
   Hash* _hashes;
 
 public:
   // Конструктор.
-  Zobrist(int size, mt* gen)
+  Zobrist(Pos size, mt* gen)
   {
     random::uniform_int_distribution<Hash> dist(numeric_limits<Hash>::min(), numeric_limits<Hash>::max());
     _size = size;
     _hashes = new Hash[size];
-    for (auto i = 0; i < size; i++)
+    for (Pos i = 0; i < size; i++)
       _hashes[i] = dist(*gen);
   }
   // Конструктор копирования.
@@ -39,9 +39,9 @@ public:
   {
     delete _hashes;
   }
-  // Возвращает хеш Зобриста элемента num.
-  Hash getHash(size_t num)
+  // Возвращает хеш Зобриста элемента pos.
+  Hash getHash(Pos pos)
   {
-    return _hashes[num];
+    return _hashes[pos];
   }
 };
