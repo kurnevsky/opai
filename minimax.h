f65df893c7f6e3d620c3720691c3d7a12dd9f318
kurnevsky <kurnevsky@gmail.com>
2014-03-17 23:24:02 +0300

    Code cleanup.

--- minimax.h
+++ minimax.h
@@ -1,12 +1,12 @@
 ﻿#pragma once
 
-#include "config.h"
-#include "basic_types.h"
 #include "field.h"
 #include "trajectories.h"
 
 using namespace std;
 
 int alphabeta(Field* field, int depth, int pos, Trajectories* last, int alpha, int beta, int* emptyBoard);
+
 int getEnemyEstimate(Field** fields, int** emptyBoards, int maxThreads, Trajectories* last, int depth);
+
 int minimax(Field* field, int depth);
--- mtdf.h
+++ mtdf.h
@@ -1,8 +1,5 @@
 ﻿#pragma once
 
-#include "config.h"
-#include "basic_types.h"
 #include "field.h"
-#include "trajectories.h"
 
 int mtdf(Field* field, int depth);
